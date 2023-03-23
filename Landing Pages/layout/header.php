f4f36e12691d5560856cf1500fef1f619b72f6c1
Kennycsm19 <112910447+Kennycsm19@users.noreply.github.com>
2023-03-23 18:40:53 -0500

    Add files via upload

---
+++ Landing Pages/layout/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>Título de la página</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descripción de la página">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/benefit.css">
+    <link rel="stylesheet" href="./css/ad.css">
+    <link rel="stylesheet" href="./css/promo.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" media="(min-width: 465px)" href="./css/tablets.css" />
+    <link rel="stylesheet" media="(min-width: 1024px)" href="./css/laptop.css" />
+  </head>
+
+  <header>
+
+    <div class="logo-header">
+      <div class="header-logo"></div>
+      <div class="btn-group">
+       <div class="t-top"></div>
+     </div>
+   </div>
+
+
+  </header>
+  <nav class="top">
+    <ul>
+      <li><a href="#">INICIO</a></li>
+      <li><a href="#">BENEFICIOS</a></li>
+      <li><a href="#">PLANES</a></li>
+      <li><a href="#">PREGUNTASS FRECUENTES</a></li>
+      <li><a href="#">PUBLICIDAD EN GOOGLE ADS</a></li>
+      <li><a href="#">CONTÁCTANOS</a></li>
+    </ul>
+  </nav>
+
+  <body>
+    <div id="loading">
+       <img src="https://emkt.pe/img/icons/loader.GIF" alt="Cargando...">
+     </div>
---
+++ Landing Pages/layout/lastcontact.php
@@ -0,0 +1,15 @@
+<section id="lastcontact">
+  <div class="lastcontact-img">
+    <figure>
+      <img src="" alt="">
+    </figure>
+  </div>
+  <div class="lastcontact-text">
+    <p>Llámanos o escríbenos hoy mismo y recibe<span>¡Asesoría Gratuita!</span></p>
+    <p>Contáctanos y un asesor nuestro te ayudará a elegir la mejor opción para optimizar la imagen de tu negocio en internet.</p>
+    <div class="lastlinks">
+      <a href="#"><span></span>(01) 642-9942</a>
+      <a href="#"><span></span>info@host-n-web.com</a>
+    </div>
+  </div>
+</section>
